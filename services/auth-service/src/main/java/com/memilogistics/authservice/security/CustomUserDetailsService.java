6571989a426b726711f5b9d310b65f6b943421c7
kbrom <kibrommehari121@gmail.com>
2026-04-13 23:27:44 +0300

    custom userdetails service

---
+++ services/auth-service/src/main/java/com/memilogistics/authservice/security/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package com.memilogistics.authservice.security;
+
+import com.memilogistics.authservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NonNull;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    @NonNull
+    public UserDetails loadUserByUsername(@NonNull String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + email));
+    }
+}
