f2099d1356a25c6d85e0fa73905a6acac6eb5d32
kbrom <kibrommehari121@gmail.com>
2026-04-13 23:27:01 +0300

    user repository

---
+++ services/auth-service/src/main/java/com/memilogistics/authservice/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package com.memilogistics.authservice.repository;
+
+import com.memilogistics.authservice.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+    Optional<User> findByEmail(String email);
+    Optional<User> findByEmailAndPassword(String email, String password);
+
+
+}
