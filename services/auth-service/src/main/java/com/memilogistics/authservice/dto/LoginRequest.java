9273b6d92f1eb1313559d663425f63e9bfa0a97d
kbrom <kibrommehari121@gmail.com>
2026-05-03 16:49:55 +0300

    added validation

--- services/auth-service/src/main/java/com/memilogistics/authservice/dto/LoginRequest.java
+++ services/auth-service/src/main/java/com/memilogistics/authservice/dto/LoginRequest.java
@@ -1,10 +1,15 @@
 package com.memilogistics.authservice.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class LoginRequest {
+    @Email
     private String email;
+    @NotBlank
+    @Size(min = 8, max = 20)
     private String password;
-    private String confirmPassword;
 }
