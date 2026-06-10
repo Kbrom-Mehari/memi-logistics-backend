03f52a314956e781e0e1b503b0491b139b5a1ab2
kbrom <kibrommehari121@gmail.com>
2026-06-10 13:16:32 +0300

    added admin existense check logic

--- auth-service/src/main/java/com/memilogistics/authservice/repository/UserRepository.java
+++ auth-service/src/main/java/com/memilogistics/authservice/repository/UserRepository.java
@@ -1,6 +1,7 @@
 package com.memilogistics.authservice.repository;
 
 import com.memilogistics.authservice.entity.User;
+import com.memilogistics.authservice.enums.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +11,8 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByEmail(String email);
     Optional<User> findByEmailAndPassword(String email, String password);
+    boolean existsByEmail(String email);
+    boolean existsByRole(Role role);
 
 
 }
